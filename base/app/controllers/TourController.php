5384f60eadd5ef92f811173e7a7b98797d200b7a
Pham van ha <haphamnvb3@gmail.com>
2025-11-21 12:26:57 +0700

    update tourController

--- base/app/controllers/TourController.php
+++ base/app/controllers/TourController.php
@@ -1,11 +1,12 @@
-<?php 
+<?php
 namespace App\Controllers;
 use App\Models\TourModel;
 
 class TourController extends BaseController
 {
     public $tour;
-    public function __construct(){
+    public function __construct()
+    {
         $this->tour = new TourModel();
     }
     public function getTours()
@@ -19,82 +20,86 @@ class TourController extends BaseController
     }
     public function postTour()
     {
-        $error =[];
-        if(empty($_POST['name'])){
+        $error = [];
+        if (empty($_POST['name'])) {
             $error['name'] = "Tên tour không được để trống";
         }
-        if(empty($_POST['description'])){
+        if (empty($_POST['description'])) {
             $error['description'] = "mo ta không được để trống";
         }
-        if(empty($_POST['price'])){
+        if (empty($_POST['price'])) {
             $error['price'] = "Giá không được để trống";
         }
-        if(empty($_POST['days'])){
+        if (empty($_POST['days'])) {
             $error['days'] = "so ngay không được để trống";
         }
-        if(empty($_POST['status'])){
+        if (!isset($_POST['status']) || $_POST['status'] === "") {
             $error['status'] = "trang thai không được để trống";
         }
-        if(count($error)>=1){
-            redirect('errors',  $error, 'add-tour');
-        }else{
-                
-                $check = $this->tour->addTour(
-                    null,
-                    $_POST['name'],
-                    $_POST['description'],
-                    $_POST['price'],
-                    $_POST['days'],
-                    $_POST['status'],
-                );
-                if ($check){
-                    redirect('success',  "Thêm thành công", 'list-tours');
-                }
+        if (count($error) >= 1) {
+            redirect('errors', $error, 'add-tour');
+        } else {
 
+            $check = $this->tour->addTour(
+                null,
+                $_POST['name'],
+                $_POST['description'],
+                $_POST['price'],
+                $_POST['days'],
+                $_POST['status'],
+            );
+            if ($check) {
+                redirect('success', "Thêm thành công", 'list-tours');
             }
-        
+
+        }
+
     }
-        public function deleteTour($id){
+    public function deleteTour($id)
+    {
         $check = $this->tour->deleteTour($id);
-        if($check){
-            redirect('success',  "Xóa thành công", 'list-tours');
+        if ($check) {
+            redirect('success', "Xóa thành công", 'list-tours');
         }
     }
 
-        public function detailTour($id){
+    public function detailTour($id)
+    {
         $detail = $this->tour->getTourById($id);
         return $this->render('tour.editTour', compact('detail'));
     }
-    public function editTour($id){
-        if (isset($_POST['btn-submit'])){
+    public function editTour($id)
+    {
+        if (isset($_POST['btn-submit'])) {
             $error = [];
-//            echo 123;
+            //            echo 123;
             // validate rỗng
-        if(empty($_POST['name'])){
-            $error['name'] = "Tên tour không được để trống";
-        }
-        if(empty($_POST['description'])){
-            $error['description'] = "mo ta không được để trống";
-        }
-        if(empty($_POST['price'])){
-            $error['price'] = "Giá không được để trống";
-        }
-        if(empty($_POST['days'])){
-            $error['days'] = "so ngay không được để trống";
-        }
-            $route = 'detail-tour/'.$id;
-            if(count($error)>=1){
-                redirect('errors',  $error, $route);
-            }else{
+            if (empty($_POST['name'])) {
+                $error['name'] = "Tên tour không được để trống";
+            }
+            if (empty($_POST['description'])) {
+                $error['description'] = "mo ta không được để trống";
+            }
+            if (empty($_POST['price'])) {
+                $error['price'] = "Giá không được để trống";
+            }
+            if (empty($_POST['days'])) {
+                $error['days'] = "so ngay không được để trống";
+            }
+            $route = 'detail-tour/' . $id;
+            if (count($error) >= 1) {
+                redirect('errors', $error, $route);
+            } else {
                 $check = $this->tour->editTour(
-                     $id,
-                     $_POST['tour_name'],
-                      $_POST['description'], 
-                      $_POST['price'],
-                      $_POST['days'],
-                      $_POST['status']);
-                if ($check){
-                    redirect('success',  "Sửa thành công", 'list-tours');
+                    $id,
+                    $_POST['tour_name'],
+                    $_POST['description'],
+                    $_POST['price'],
+                    $_POST['days'],
+                    $_POST['status']
+                );
+                if ($check) {
+                    redirect('success', "Sửa thành công", 'list-tours');
                 }
 
             }
